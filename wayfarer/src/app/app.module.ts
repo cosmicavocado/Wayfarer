ddfd67b439f67f6c4037b8527cba09134e791154
Mariah Legg <mariah.legg@gmail.com>
2022-01-21 21:57:04 -0500

    Add images and posts to cities.ts. Create post component.

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CitiesComponent } from './cities/cities.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CitiesComponent
+    CitiesComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
--- wayfarer/src/app/cities/cities.ts
+++ wayfarer/src/app/cities/cities.ts
@@ -1,8 +1,62 @@
 export const CITIES = [
     {
         id: 1,
-        name: 'San Franciso',
-        country: 'USA',
-        image: '../../assets/images/golden-gate.png'
+        name: "San Franciso",
+        country: "USA",
+        image: "../../assets/images/golden-gate.png",
+        posts: [
+            {
+                id: 1,
+                date: "01/21/22",
+                title: "Golden Gate",
+                body: "Look at that ding-dang bridge! Very cash money."
+            },
+            {
+                id: 2,
+                date: "01/21/20",
+                title: "Golden Gate",
+                body: "Look at that ding-dang bridge! Very cash money."
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "London",
+        country: "United Kingdom",
+        image: "../../assets/images/london-eye.png",
+        posts: [
+            {
+                id: 1,
+                date: "12/4/21",
+                title: "Eye came. Eye saw. Eye conquered.",
+                body: "This wheel really caught my eye! I wasn't about it at first but I'm slowly coming around."
+            },
+            {
+                id: 2,
+                date: "12/4/21",
+                title: "Spent Big Benjamins to See Big Ben",
+                body: "Is it safe to say the people who work near Big Ben are...working around the clock?"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Gibraltar",
+        country: "British Overseas Territory",
+        image: "../../assets/images/gibraltar-monkey.png",
+        posts: [
+            {
+                id: 1,
+                date: "12/4/21",
+                title: "Monkey Business Confirmed in Gibraltar",
+                body: "I just visited Gibraltar and it was more fun than a barrel of monkies! Truly it was bananas."
+            },
+            {
+                id: 2,
+                date: "4/3/20",
+                title: "Monkey Business Confirmed in Gibraltar",
+                body: "I just visited Gibraltar and it was more fun than a barrel of monkies! Truly it was bananas."
+            }
+        ]
     }
 ]
