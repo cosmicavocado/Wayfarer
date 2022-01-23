bfea7cd174e813a54c70830458789e99e4306242
Mariah Legg <mariah.legg@gmail.com>
2022-01-22 20:14:49 -0500

    Add author to cities.ts.

--- wayfarer/src/app/cities/cities.ts
+++ wayfarer/src/app/cities/cities.ts
@@ -8,12 +8,14 @@ export const CITIES = [
             {
                 id: 1,
                 date: "1/21/22",
+                author: "Mariah Legg",
                 title: "Golden Gate",
                 body: "Look at that ding-dang bridge! The suspension is killing me."
             },
             {
                 id: 2,
                 date: "8/30/21",
+                author: "Eric Brown",
                 title: "I’m a San Fan-cisco",
                 body: "I hate to say good bay to San Francisco but I'm trying to quit gold-turkey."
             }
@@ -28,12 +30,14 @@ export const CITIES = [
             {
                 id: 1,
                 date: "1/1/22",
+                author: "Mariah Legg",
                 title: "Eye came. Eye saw. Eye conquered.",
                 body: "This wheel really caught my eye! I wasn't about it at first but I'm slowly coming around."
             },
             {
                 id: 2,
                 date: "11/21/21",
+                author: "Eric Brown",
                 title: "Spent Big Benjamins to See Big Ben",
                 body: "Is it safe to say the people who work near Big Ben are...working around the clock?"
             }
@@ -48,12 +52,14 @@ export const CITIES = [
             {
                 id: 1,
                 date: "1/8/22",
+                author: "Eric Brown",
                 title: "Monkey Business Confirmed in Gibraltar",
                 body: "I just visited Gibraltar and it was more fun than a barrel of monkies! Truly it was bananas."
             },
             {
                 id: 2,
                 date: "4/3/21",
+                author: "Mariah Legg",
                 title: "Gibraltar Rock(s)",
                 body: "My trip to see the Rock of Gibraltar was truly sublime-stone!"
             }
