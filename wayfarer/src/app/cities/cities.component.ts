3dd0d24f62026c41ce941e52e9c2946221958067
Mariah Legg <mariah.legg@gmail.com>
2022-01-23 21:00:01 -0500

    Add open weather integration to city page.

--- wayfarer/src/app/cities/cities.component.ts
+++ wayfarer/src/app/cities/cities.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 import { CITIES } from './cities';
 
 @Component({
@@ -11,8 +14,11 @@ export class CitiesComponent implements OnInit {
 
   city: any;
   posts: any;
+  weather: any;
+  subject = new Subject();
+  key: string = 'ea0ac3979e7ffe0003ba30e895aea3b5';
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.route.paramMap
@@ -30,7 +36,13 @@ export class CitiesComponent implements OnInit {
         b = b.date.split('/').reverse().join('');
         return b - a;
       })
+      this.weather = this.getWeather(this.city.name);
     })
   }
 
+    getWeather(name: string) {
+    this.http.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${this.key}&&units=metric`)
+    .subscribe((response: any) => this.weather = response);
+  }
 }
